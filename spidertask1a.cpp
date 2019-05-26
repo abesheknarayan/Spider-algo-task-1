18c934949fd7050e429638f2317a796cef26280a
abesheknarayan <49567412+abesheknarayan@users.noreply.github.com>
2019-05-26 19:34:06 +0530

    Add files via upload

---
+++ spidertask1a.cpp
@@ -0,0 +1,177 @@
+#include<bits/stdc++.h>
+using namespace std;
+char convertbinary(int no,int n)
+{ int i=0;
+int a[1000];
+	while(no>0)
+	{
+	  int r=no%2;
+	  a[i]=r;
+	  i++;
+	  no=no/2;	
+	}
+	for(int j=0;j<i;j++)
+	{
+		cout<<a[n-j-1];
+	}
+}
+int convertnumber(char s[],int n){
+   int no=0;
+	for(int i=n-1;i>=0;i--)
+	{
+		if(s[i]=='1')
+		{
+			no+=pow(2,n-1-i);
+		}
+	}
+	return no;
+}
+int binaryadd(char s[],int n)
+{   int p=0;
+	int carry=0;
+	int i;
+	for(i=n-1;i>=0;i--)
+	{ if(i==n-1)   
+	    {
+		if(carry==0)
+		{
+		if(s[i]=='0')
+		{
+			s[i]='1';
+			carry=0;
+		}
+		else if(s[i]=='1'){
+			s[i]='0';
+			carry=1;
+		}
+	    }
+	    
+		}
+	
+
+	else if(i==0)
+	{
+		if(carry==1)
+		{
+			if(s[i]=='1')
+			{ 
+		      p=1;
+			  break;		
+			}
+		}
+	}
+	else{
+		if(carry==1)
+	{
+		if(s[i]=='1')
+		{
+			s[i]='0';
+			carry=1;
+		}
+		else if(s[i]=='0')
+		{
+			s[i]='1';
+			carry=0;
+		}
+		
+	}
+		
+	
+	}
+	
+	}
+     
+	return p;
+	
+}
+int binarysub(char s[],int n)
+{ int b=0;
+  int p=0;
+	for(int i=n-1;i>=0;i--)
+	{
+		if(i==n-1)
+		{
+			if(s[i]=='1')
+			{
+				s[i]='0';
+				b=0;
+		    }
+		    else if(s[i]=='0')
+		    {
+		    	s[i]='1';
+		    	b=1;
+			}
+		}
+		else if(i==0)
+		{
+			if(b==1)
+			{
+				if(s[i]=='1')
+				{
+				  s[i]='0';
+				  b=0;
+				  p=1;
+				  
+				}
+			}
+		}
+		else
+	    {
+	    	if(b==1)
+	    	{
+	    		if(s[i]=='1')
+	    		{
+	    			s[i]='0';
+	    			b=0;
+				}
+				else if(s[i]=='0')
+				{
+					s[i]='1';
+					b=1;
+				}
+			}
+			
+			
+		}
+	}
+	return p;
+	
+}
+
+int main()
+{
+	long int n;
+	int i;
+	cout<<"Enter the length of the string "<<endl;
+	cin>>n;
+	char s[110],s2[110];
+
+    for( i=0;i<n;i++)
+    {
+    	cin>>s[i];
+    	s2[i]=s[i];
+	}
+	int p=binaryadd(s,n);
+	int p2=binarysub(s2,n);
+	if(p==0&&p2==0)
+	{
+	  for(i=0;i<n;i++)
+	  {
+	  	cout<<s[i];
+		  }	
+		  cout<<" ";
+		  for(i=0;i<n;i++)
+		  {
+		   cout<<s2[i];
+		  }
+		  
+	}
+    else{
+    	cout<<"-1";
+	}
+	
+
+	
+	
+	return 0;
+}
